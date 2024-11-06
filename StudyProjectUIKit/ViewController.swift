aa803cc93a13f019eae4fabe84b2f4b1828e23e0
Roman Khovalko <hromnik@me.com>
2024-11-06 15:39:33 +0200

    Initial Commit

---
+++ StudyProjectUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyProjectUIKit
+//
+//  Created by iHRomnik on 06.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StudyProjectUIKitTests/StudyProjectUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyProjectUIKitTests.swift
+//  StudyProjectUIKitTests
+//
+//  Created by iHRomnik on 06.11.2024.
+//
+
+import Testing
+@testable import StudyProjectUIKit
+
+struct StudyProjectUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
